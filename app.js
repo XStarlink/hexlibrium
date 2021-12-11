530ba6876a767abe6fbe1a5496f45afa6fad39ed
Vinicius de Moraes Nascimento <vinicci.exe@gmail.com>
2021-12-11 17:04:01 +0000

    back

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'hexlibrium/public');
+const publicPath = path.join(__dirname, '..', 'hexlibrium/src');
 const port = process.env.PORT || 1000;
 
-// //
+//
 // app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname,'views'))
-
-app.use(express.static(path.join(__dirname,'src')));
-
-// //
+//
 
 app.use(express.static(publicPath));
 app.use(express.static('src'));
